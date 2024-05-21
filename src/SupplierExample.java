f4269a312a96bf4fce58fc39fccd79e29fad6213
unknown <philippediniz@outlook.com>
2024-05-21 12:31:39 -0300

    Classe Supplier Exemplo

---
+++ src/SupplierExample.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+//Representa uam operação que não aceita nenhum argumento e retorna um resultado do tipo t.
+// É comumente usada para criar ou fornecer novos objetos de um determinado tipo.
+public class SupplierExample {
+
+    public static void main(String[] args) {
+
+        // Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
+        Supplier<String> saudacao = () -> "Ola, seja bem vindo(a)!";
+
+        // Usar o Supplier para obter  uma lista com 5 saudaçoes
+        List<String> listaSaudacoes = Stream.generate(saudacao)
+                .limit(5)
+                .collect(Collectors.toList());
+
+        // Imprimir as saudações geradas
+        listaSaudacoes.forEach(s -> System.out.println(s));
+
+
+    }
+
+
+}
